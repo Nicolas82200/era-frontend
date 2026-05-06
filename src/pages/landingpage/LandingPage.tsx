ac1577a5771c339a9874e2e546d0ce10b8ec9952
FARVACQUE-L <laurent.fvq@gmail.com>
2026-05-06 16:09:26 +0200

    logo background slogan description and counter event and periode on landingpage .tsx and css in progress

--- src/pages/landingpage/LandingPage.tsx
+++ src/pages/landingpage/LandingPage.tsx
@@ -1,13 +1,54 @@
+import { useState, useEffect } from "react";
+
+import Logo from "../../assets/images/company.png";
+import voyage from "../../assets/images/voyage-temps.png";
+
 import "./LandingPage.css";
 
+import type { periodsType } from "../../types/periodsType";
+// import { eventsType } from "../../types/eventsType";
+
 function LandingPage() {
+	const [periods, setPeriods] = useState<periodsType[]>([]);
+	// const [events, setEvents] = useState<eventsType[]>([]);
+
+	useEffect(() => {
+		fetch("http://localhost:3310/periods").then((response) =>
+			response.json().then(setPeriods),
+		);
+		// fetch("http://localhost:3310/events").then((response) => response.json().then(setEvents));
+	}, []);
+
 	return (
-		<>
-			<h1 className="LandingPage-Title">Era</h1>
-			<h2 className="LandingPage-Slogan">
+		<div className="LandingPage-Global">
+			<div className="LandingPage-Background">
+				<img
+					className="LandingPage-BackgroundImg"
+					src={voyage}
+					alt="spirale du temps"
+				/>
+			</div>
+			<div className="LandingPage-ContainerLogo">
+				<img className="LandingPage-Logo" src={Logo} alt="Era logo" />
+			</div>
+			<h1 className="LandingPage-Slogan">
 				L'histoire ne se lit plus, elle se vit !
-			</h2>
-		</>
+			</h1>
+			<p className="LandingPage-Describe">
+				De l'ère Precambriène à la conquête inter-galactique ! Revivez les
+				grands évenements Terriens.
+			</p>
+			<div className="LandingPage-Counters">
+				<div className="LandingPage-Counter">
+					<div className="LandingPage-CounterNumber">{periods.length}</div>
+					<div className="LandingPage-CounterName">Époques</div>
+				</div>
+			</div>
+			{/* <div className="LandingPage-Counter">
+				<div className="LandingPage-CounterNumber">{events.length}</div>
+				<div className="LandingPage-CounterName">Événements</div>
+			</div> */}
+		</div>
 	);
 }
 
