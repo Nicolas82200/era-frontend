308d4fea8b231a2c599539335e24cbc431bf2069
FARVACQUE-L <laurent.fvq@gmail.com>
2026-05-06 22:27:29 +0200

    Landing Page finish

--- src/pages/landingpage/LandingPage.tsx
+++ src/pages/landingpage/LandingPage.tsx
@@ -1,22 +1,26 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 import Logo from "../../assets/images/company.png";
 import voyage from "../../assets/images/voyage-temps.png";
 
+import type { eventsType } from "../../types/eventsType";
+import type { periodsType } from "../../types/periodsType";
+
 import "./LandingPage.css";
 
-import type { periodsType } from "../../types/periodsType";
-// import { eventsType } from "../../types/eventsType";
-
 function LandingPage() {
+	const navigate = useNavigate();
 	const [periods, setPeriods] = useState<periodsType[]>([]);
-	// const [events, setEvents] = useState<eventsType[]>([]);
+	const [events, setEvents] = useState<eventsType[]>([]);
 
 	useEffect(() => {
 		fetch("http://localhost:3310/periods").then((response) =>
 			response.json().then(setPeriods),
 		);
-		// fetch("http://localhost:3310/events").then((response) => response.json().then(setEvents));
+		fetch("http://localhost:3310/events").then((response) =>
+			response.json().then(setEvents),
+		);
 	}, []);
 
 	return (
@@ -31,11 +35,13 @@ function LandingPage() {
 			<div className="LandingPage-ContainerLogo">
 				<img className="LandingPage-Logo" src={Logo} alt="Era logo" />
 			</div>
-			<div className="LandingPage-SloganDescribe">
+			<div className="LandingPage-SloganGlobal">
 				<h2 className="LandingPage-SloganBegining">
 					L'histoire ne se lit plus...
 				</h2>
 				<h2 className="LandingPage-SloganEnding">Elle se vit !</h2>
+			</div>
+			<div className="LandingPage-DescribeGlobal">
 				<p className="LandingPage-Describe">
 					De l'ère Précambriène à la conquête inter-galactique !
 				</p>
@@ -43,16 +49,25 @@ function LandingPage() {
 					Revivez les grands évenements Terriens.
 				</p>
 			</div>
-			<div className="LandingPage-Counters">
-				<div className="LandingPage-Counter">
-					<div className="LandingPage-CounterNumber">{periods.length}</div>
-					<div className="LandingPage-CounterName">Époques</div>
+			<div className="LandingPage-CountersGlobal">
+				<div className="LandingPage-Counters">
+					<div className="LandingPage-Counter">
+						<div className="LandingPage-CounterNumber">{periods.length}</div>
+						<div className="LandingPage-CounterName">Époques</div>
+					</div>
+					<div className="LandingPage-Counter">
+						<div className="LandingPage-CounterNumber">{events.length}</div>
+						<div className="LandingPage-CounterName">Événements</div>
+					</div>
 				</div>
+				<button
+					className="LandingPage-Button"
+					type="button"
+					onClick={() => navigate("/era/")}
+				>
+					Commencez le voyage
+				</button>
 			</div>
-			{/* <div className="LandingPage-Counter">
-				<div className="LandingPage-CounterNumber">{events.length}</div>
-				<div className="LandingPage-CounterName">Événements</div>
-			</div> */}
 		</div>
 	);
 }
