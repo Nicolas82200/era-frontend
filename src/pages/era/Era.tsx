2861d98873f54129f9c141fdfdd418073b187d2e
Leo Fleury <leofleury72@gmail.com>
2026-05-06 18:03:57 +0200

    update css

--- src/pages/era/Era.tsx
+++ src/pages/era/Era.tsx
@@ -1,5 +1,5 @@
 import UsePeriods from "../../services/UsePeriods";
-import styles from "./Era.module.css";
+
 import "./Era.css";
 
 function Era() {
@@ -10,15 +10,13 @@ function Era() {
 			{allPeriods
 				?.map((period) => (
 					<section key={period.id} className="section_era">
-						<p className={`${styles[period.name]}-sectionBadge`}>
-							{period.time.start}
-						</p>
+						<p className={`${period.name}-sectionBadge`}>{period.time.start}</p>
 						<h2 className="section_era-title">{period.name}</h2>
 						<p>{period.introduction}</p>
 						<p>{period.description}</p>
 					</section>
 				))
-				.slice(0, 1)}
+				.slice(1, 2)}
 		</>
 	);
 }
