57b5164c413baaefb951f6ec40d298dfaf5a00b2
FARVACQUE-L <laurent.fvq@gmail.com>
2026-05-06 11:32:54 +0200

    start landingPage title + slogan

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import LandingPage from "./pages/landingpage/LandingPage";
+
+function App() {
+	return <LandingPage />;
+}
+
+export default App;
---
+++ src/pages/landingpage/LandingPage.tsx
@@ -0,0 +1,12 @@
+function LandingPage() {
+	return (
+		<>
+			<h1 className="LandingPage-Title">Era</h1>
+			<h2 className="LandingPage-Slogan">
+				L'histoire ne se lit plus, elle se vit !
+			</h2>
+		</>
+	);
+}
+
+export default LandingPage;
