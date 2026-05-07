3cd03fd324f43ebd19be487a6e5d4f5a2969d120
Leo Fleury <leofleury72@gmail.com>
2026-05-07 11:01:25 +0200

    component done

--- src/pages/era/Era.tsx
+++ src/pages/era/Era.tsx
@@ -1,23 +1,37 @@
+import { useState } from "react";
 import UsePeriods from "../../services/UsePeriods";
 // import prehistoireImg from "../../assets/images/PrehistoryImg.png";
 import "./Era.css";
 
 function Era() {
+	const [eraId, setEraId] = useState<number>();
 	const allPeriods = UsePeriods();
+	const id = allPeriods?.map((periods) => periods.id);
+
+	// const carousel = allPeriods?.filter((periods) => periods.id);
+	console.log(id);
+	// allPeriods?.map();
+	// setEraId();
 
 	return (
 		<>
-			{allPeriods
-				?.map((period) => (
-					<section key={period.id} className={`${period.name}-section_era`}>
-						<p className={`${period.name}-sectionBadge`}>{period.time.start}</p>
+			{allPeriods?.map((period) => (
+				<section key={period.id} className="section_era-container">
+					<section className={`${period.index_name}-section_era`}>
+						<p className={`${period.index_name}-sectionBadge`}>
+							{period.time.start}
+						</p>
 						<div className="div_era">
-							<h2 className="section_era-title">{period.name}</h2>
-							<p>{period.introduction}</p>
+							<h2 className={`${period.index_name}-section_era-title`}>
+								{period.name}
+							</h2>
+							<p className={`${period.index_name}-section_era-desc`}>
+								{period.introduction}
+							</p>
 						</div>
 					</section>
-				))
-				.slice(1, 2)}
+				</section>
+			))}
 		</>
 	);
 }
